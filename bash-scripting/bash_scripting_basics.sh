9bdd398e31b9fa7e014269f014310738f179767b
2junechung <2junechung@gmail.com>
2022-05-29 23:12:37 -0700

    adding text manipulation

--- bash-scripting/bash_scripting_basics.sh
+++ bash-scripting/bash_scripting_basics.sh
@@ -1,1 +1,30 @@
 #### bash scripting
+
+# input 
+$1 # first argument
+$2 # second argument 
+$0 # name of script 
+$? # exit status
+
+# accept input 
+read $name1 
+read $name2 
+result=$((name1 + name2)) # cannot space
+result=$((name1 / name2)) # becomes math expression $(())
+
+# ALWAYS use [[ ]] instead of []
+
+# for loop 
+for ((i=1; i<51; i++))
+do                      # do is only for "for loop"
+    echo $i 
+done
+
+# if else
+if [[ $vara -gt $varb ]]; then
+    echo X is greater than Y  
+elif [[ $vara -eq $varb ]]; then 
+    echo X is equal to Y   
+elif [[ $vara -lt $varb ]]; then 
+    echo X is less than Y  
+fi
--- bash-scripting/text_manipulation/_text_manipulation.sh
+++ bash-scripting/text_manipulation/_text_manipulation.sh
@@ -2,22 +2,30 @@
 ##### grep, awk, sed 
 
 echo SORT and UNIQ and TR and wc ...
-# sort 
+############## sort 
 sort && sort -r (reverse) 
-# uniq 
+sort -n # number sort ex. 4 , 44.5 , 5.0 (x)
+############## uniq 
 uniq && uniq -c (numbers) && uniq -i (insensitive)
-# word count
+uniq -u (only unique lines)
+############## word count
 wc -l (lines) && wc -w (words) && wc -m (char) && wc -c (bytes)
-# replace
+############## tr
 tr "," " " # replace , with space
 tr '\n' ',' # horizontal -> vertical
 tr -d " " # delete space
 tr -d '\\b' # delete with special characters -> deletes \b !! need single quote
+tr -d "[a-z]"
 # tr can only replace same amount of characters
 # tr replace is not that complete... (comparison is by character only , not whole string)
 
-# cut 
-cut -b -c etc
+############## cut 
+cut  -b -c -f # byte, char, field(table), deliminater for fields
+cut -c1,4 # only print 3rd and 7th 
+cut -c1-4 # print first ~ 4th 
+cut -c13- # infinite -> leave as empty 13-XX
+cut -d ' ' -f1-3 # "," as deliminater - 1st ~ 3rd field # default deliminator = tab
+
 
 # head and tail 
 head -n 10 && tail -n 8
@@ -26,6 +34,17 @@ head -n 10 && tail -n 8
 echo replace word using "/s" substitute sed ....
 sed -i '/s/hello/annyoung/g' # replaces hello to annyoung + case insensitive 
 
+
+######################### grep 
+echo grep only exact words ... 
+echo grep only matching word ...
+echo grep "case insensitive"...
+grep -w -i -o "helloworld" 
+grep "\-o" # grep sepcial characters / will grep "-o"
+grep -e "hello" -e "annyoung" -e "adios" # grep multiple words
+grep "\bhelloworld\b" # grep with word boundary "\b"
+
+cat workday.txt | grep -w -i -o "the\|ugliest\|contest\|dog\|world's" | sort | uniq -c -i | awk '{print $1}' | tr '\n' ','
 
 ######################### awk 
 awk '{print $1}' # default divides by spaces 
@@ -42,13 +61,3 @@ echo awk print on value filtering ...
 awk '{ if ($7 == "finding value" ) { print } }' # print for only 7th column matching value
 
 
-######################### grep 
-echo grep only exact words ... 
-echo grep only matching word ...
-echo grep "case insensitive"...
-grep -w -i -o "helloworld" 
-grep "\-o" # grep sepcial characters / will grep "-o"
-grep -e "hello" -e "annyoung" -e "adios" # grep multiple words
-grep "\bhelloworld\b" # grep with word boundary "\b"
-
-cat workday.txt | grep -w -i -o "the\|ugliest\|contest\|dog\|world's" | sort | uniq -c -i | awk '{print $1}' | tr '\n' ','
